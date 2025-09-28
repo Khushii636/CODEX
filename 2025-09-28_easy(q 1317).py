c51b0ef2df0904542c1afa8c766df59932582f2e
Khushi Choudhary <khushichowdhary18@gmail.com>
2025-09-28 19:22:34 +0530

    Update 2025-09-28_easy(q 1317).py

--- 2025-09-28_easy(q 1317).py
+++ 2025-09-28_easy(q 1317).py
@@ -1,16 +1,16 @@
 #1317. Convert Integer to the Sum of Two No-Zero Integers
 
-No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
-Given an integer n, return a list of two integers [a, b]
-where:
-a and b are No-Zero integers.
-The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
-Example 1:
-Input: n = 2
-Output: [1,1]
-Explanation: Let a = 1 and b = 1.
-Both a and b are no-zero integers, and a + = 2 = n.
-#
+#No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+#Given an integer n, return a list of two integers [a, b]
+#where:
+#a and b are No-Zero integers.
+#The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
+#Example 1:
+#Input: n = 2
+#Output: [1,1]
+#Explanation: Let a = 1 and b = 1.
+#Both a and b are no-zero integers, and a + = 2 = n.
+
 
 class Solution(object):
     def getNoZeroIntegers(self, n):
